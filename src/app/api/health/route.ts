b39cc85c93ed77f232249ee571424034fd0caab7
abraham hailegebriel <abrahamhailegebriel@email.com>
2025-09-16 19:34:49 +0300

    package.json file updated and health checker added for render

---
+++ src/app/api/health/route.ts
@@ -0,0 +1,6 @@
+// src/app/api/health/route.ts
+import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
+export async function GET() {
+  return NextResponse.json({ ok: true });
+}
